43b6e7d1ad4dc2fe945b24d1b2c6f955ca03c9e7
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2025-05-17 22:25:21 +0200

    Backend : setup Spring Boot backend

---
+++ candidatTrackerBackend/src/main/java/manage/candidatTrackerBackend/CandidatTrackerApplication.java
@@ -0,0 +1,13 @@
+package manage.candidatTrackerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatTrackerApplication.class, args);
+	}
+
+}
---
+++ candidatTrackerBackend/src/test/java/manage/candidatTrackerBackend/CandidatTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package manage.candidatTrackerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
