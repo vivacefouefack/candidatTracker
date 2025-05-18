0af4494e1f460c8e46a0f2f0c84f368a91da683c
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2025-05-19 00:53:37 +0200

    Backend : add JwToken entity to store generated JWTs

---
+++ candidatTrackerBackend/src/main/java/manage/candidatTrackerBackend/model/JwToken.java
@@ -0,0 +1,30 @@
+package manage.candidatTrackerBackend.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a JSON Web Token associated with an user.
+*/
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JwToken {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String value;
+    private boolean expire;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    private User user;
+}
