39d6fc350224290498a8c0a5f619c503f641a9f9
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2025-05-23 18:29:34 +0200

    Frontend : add route guard to protect authenticated routes

--- candiTrack/src/app/app.routes.ts
+++ candiTrack/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard]},
   { path: '**', redirectTo: 'login' }
 ];
 
---
+++ candiTrack/src/app/auth.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const token = localStorage.getItem('token');
+  if (token) {
+    return true;
+  } else {
+    const router = inject(Router);
+    router.navigate(['/login']);
+    return false;
+  }
+};
